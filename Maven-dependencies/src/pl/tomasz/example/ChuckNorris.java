28948e485044962ce1f7d0290b63cb8b7b3edc3c
tomaszszymanowski <tomasz.szymanowski@gmail.com>
2021-03-01 20:00:40 +0100

    #2

--- Maven-dependencies/src/pl/tomasz/example/ChuckNorris.java
+++ Maven-dependencies/src/pl/tomasz/example/ChuckNorris.java
@@ -1,2 +1,16 @@
-package pl.tomasz.example;public class ChuckNorris {
+package pl.tomasz.example;
+
+import io.chucknorris.client.ChuckNorrisClient;
+import io.chucknorris.client.Joke;
+
+public class ChuckNorris {
+    public static void main(String[] args) {
+
+        // create the chuck norris client
+        ChuckNorrisClient client = new ChuckNorrisClient();
+
+// get a random joke and print it
+        Joke joke = client.getRandomJoke();
+        System.out.println(joke.getValue());
+    }
 }
